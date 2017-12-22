b806418e91482391c8527ef3462506f675af7ac0
she1l <763497443@qq.com>
2017-12-22 09:14:14 +0800

    first commit

---
+++ duowan/spiders/meizi.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from duowan.items import DuowanItem
+import os
+
+
+class MeiziSpider(scrapy.Spider):
+    name = 'meizi'
+    allowed_domains = ['duowan.com']
+    _url = 'http://tu.duowan.com/m/meinv?offset='
+    offset = 330
+    start_urls = [_url + str(offset)]
+
+    def parse(self, response):
+        url_list = response.xpath('//em/a/@href').extract()
+        for url in url_list:
+            url_offset = url.split('/')[-1].split('.')[0]
+            os.mkdir('img/' + url_offset)
+            url_request = 'http://tu.duowan.com/index.php?r=show/getByGallery/&gid=' + url_offset
+            # print url
+            yield scrapy.Request(url=url_request, meta={'url': url_offset}, callback=self.parse_item)
+
+        if self.offset < 2550:  # 2550
+            self.offset += 30
+        yield scrapy.Request(self._url + str(self.offset), callback=self.parse)
+
+    def parse_item(self, response):
+        sites = json.loads(response.body_as_unicode())
+        for site in sites['picInfo']:
+            item = DuowanItem()
+            item['img_url'] = site['url']
+            item['path'] = response.meta['url']
+            yield item
